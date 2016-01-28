1c421de75b672535f7f54e33005c3ef181e80ebf
Pyunghyuk Yoo <yoophi@gmail.com>
2016-01-28 11:57:38 +0900

    import

---
+++ todo/__init__.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os.path as op
+
+from flask.ext.cors import CORS
+from flask.ext.debugtoolbar import DebugToolbarExtension
+from flask.ext.mail import Mail
+from flask.ext.oauthlib.provider import OAuth2Provider
+from flask.ext.security import SQLAlchemyUserDatastore, Security
+
+from .helpers import Flask
+from .models import db, User, Role
+
+__version__ = '0.1'
+
+# login_manager = LoginManager()
+# login_manager.session_protection = 'strong'
+# login_manager.login_view = 'auth.login'
+
+oauth = OAuth2Provider()
+cors = CORS()
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(datastore=user_datastore)
+debug_toolbar = DebugToolbarExtension()
+mail = Mail()
+
+def create_app(config_name):
+    """
+    :param config_name: developtment, production or testing
+    :return: flask application
+
+    flask application generator
+    """
+    template_folder = op.join(op.dirname(op.abspath(__file__)), 'templates')
+    app = Flask(__name__, template_folder=template_folder)
+    app.config.from_yaml(app.root_path)
+    app.config.from_heroku()
+
+    cors.init_app(app)
+    db.init_app(app)
+    # login_manager.init_app(app)
+    oauth.init_app(app)
+    security.init_app(app)
+    debug_toolbar.init_app(app)
+    mail.init_app(app)
+
+    from .main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint)
+
+    # from .auth import auth as auth_blueprint
+    #
+    # app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .api_1_0 import api as api_1_0_blueprint
+
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
+    return app
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     """Hook for Flask-Login to load a User instance from a user ID."""
+#     return User.query.get(user_id)
---
+++ todo/models.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+sqlalchemy model
+"""
+from flask.ext.security import RoleMixin, UserMixin
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, synonym
+
+db = SQLAlchemy()
+
+# Define models
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('roles.id')))
+
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    current_login_at = db.Column(db.DateTime())
+    current_login_ip = db.Column(db.String(255))
+    login_count = db.Column(db.Integer)
+
+
+class Client(db.Model):
+    __tablename__ = 'clients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(40))
+
+    # human readable description, not required
+    description = db.Column(db.Unicode(400))
+
+    # creator of the client, not required
+    user_id = db.Column(
+        db.Integer,
+        ForeignKey('users.id'),
+        nullable=False,
+    )
+    user = relationship('User')
+
+    client_id = db.Column(db.Unicode(40), unique=True)
+    client_secret = db.Column(db.Unicode(55), index=True, nullable=False)
+
+    # public or confidential
+    is_confidential = db.Column(db.Boolean)
+
+    redirect_uris_text = db.Column(db.UnicodeText)
+    default_scopes_text = db.Column(db.UnicodeText)
+
+    @property
+    def client_type(self):
+        if self.is_confidential:
+            return 'confidential'
+        return 'public'
+
+    @property
+    def redirect_uris(self):
+        if self.redirect_uris_text:
+            return self.redirect_uris_text.split()
+        return []
+
+    @property
+    def default_redirect_uri(self):
+        return self.redirect_uris[0]
+
+    @property
+    def default_scopes(self):
+        if self.default_scopes_text:
+            return self.default_scopes_text.split()
+        return []
+
+    def __repr__(self):
+        return u'<{self.__class__.__name__}: {self.id}>'.format(self=self)
+
+
+class Grant(db.Model):
+    __tablename__ = 'grants'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # user_id = db.Column(db.Unicode(200))dd
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False,
+    )
+    user = relationship('User')
+
+    client_id = db.Column(
+        db.Unicode(40),
+        db.ForeignKey('clients.client_id'),
+        nullable=False,
+    )
+    client = relationship('Client')
+
+    code = db.Column(db.Unicode(255), index=True, nullable=False)
+
+    redirect_uri = db.Column(db.Unicode(255))
+    expires = db.Column(db.DateTime)
+
+    _scopes = db.Column(db.UnicodeText)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def scopes(self):
+        if self._scopes:
+            return self._scopes.split()
+        return []
+
+
+class Token(db.Model):
+    __tablename__ = 'tokens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(
+        db.Unicode(40),
+        db.ForeignKey('clients.client_id'),
+        nullable=False,
+    )
+    client = relationship('Client')
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False,
+    )
+    user = relationship('User')
+
+    # currently only bearer is supported
+    token_type = db.Column(db.Unicode(40))
+
+    access_token = db.Column(db.Unicode(255), unique=True)
+    refresh_token = db.Column(db.Unicode(255), unique=True)
+    expires = db.Column(db.DateTime)
+    _scopes = db.Column(db.UnicodeText)
+
+    @property
+    def scopes(self):
+        if self._scopes:
+            return self._scopes.split()
+        return []
+
+    def _get_scope(self):
+        if self._scopes:
+            return self._scopes.split()
+        return []
+
+    def _set_scope(self, scope):
+        if scope:
+            scope = scope
+        self._scopes = scope
+
+    scope_descriptor = property(_get_scope, _set_scope)
+    scope = synonym('_scopes', descriptor=scope_descriptor)
+
+    def __repr__(self):
+        return u'<{self.__class__.__name__}: {self.id}>'.format(self=self)
