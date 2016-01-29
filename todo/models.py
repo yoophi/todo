c8303945c6d4eb57ce385196c84294c123b23721
Pyunghyuk Yoo <yoophi@gmail.com>
2016-01-29 23:32:10 +0900

    added Todo model

--- todo/models.py
+++ todo/models.py
@@ -182,3 +182,25 @@ class Token(db.Model):
 
     def __repr__(self):
         return u'<{self.__class__.__name__}: {self.id}>'.format(self=self)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True)
+    priority = db.Column(db.Integer, default=3)
+    is_completed = db.Column(db.Boolean(), default=False)
+
+    user_id = db.Column(
+        db.Integer,
+        ForeignKey('users.id'),
+        nullable=False,
+    )
+    user = relationship('User', backref='todos')
+
+    created_at = db.Column(db.DateTime())
+    updated_at = db.Column(db.DateTime())
+
+    def __repr__(self):
+        return u'<{self.__class__.__name__}: {self.title}>'.format(self=self)
