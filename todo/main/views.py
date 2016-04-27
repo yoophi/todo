aa0c4c3c2afae199f4dd1808bbaaf76800c1357d
Pyunghyuk Yoo <yoophi@gmail.com>
2016-04-27 23:17:40 +0900

    add swagger.json sample and route

--- todo/main/views.py
+++ todo/main/views.py
@@ -1,11 +1,21 @@
-from flask import render_template
-# from flask.ext.login import current_user
+import os
+import json
+from flask import render_template, jsonify, current_app
+from flask.ext.cors import cross_origin
 
 from . import main
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    # return 'Hello, %s' % (current_user.email if hasattr(current_user, 'email') else 'world')
     return render_template('index.html')
 
+
+@main.route('/swagger.json', methods=['GET'])
+@cross_origin()
+def swagger_json():
+    swagger_file = os.path.join(current_app.root_path, 'swagger.json')
+    with open(swagger_file) as data_file:
+        data = json.load(data_file)
+
+    return jsonify(data)
